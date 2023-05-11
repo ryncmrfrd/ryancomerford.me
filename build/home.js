51e71309d432fcabd9f325e54c8f63259e088c84
Ryan Comerford <ryncmrfrd@outlook.com>
2023-05-11 18:51:26 +1000

    New website version, fit for 2023
    - Revamped home and 404 pages
    - Change of style ethos
    - Addition of acheivements, education, and personal interests
    - Improved responsiveness

--- build/home.js
+++
@@ -1,7 +0,0 @@
-// all of the code creating the scrolling pages effect
-const pg = new pageination("#elem", {
-	dots: { type: "right", theme: "dark" },
-	onPageChange: (pageName, pageIndex) => {
-		window.location.hash = pageName;
-	}
-});
--- build/redirect.js
+++
@@ -1,8 +0,0 @@
-const changeRedirectNum = (elem) => {
-    var currentNum = Number(elem.innerText)
-    currentNum-1 == 0 ?
-        window.location.replace('/') :
-        (elem.innerText = currentNum-1, setTimeout(() => changeRedirectNum(elem), 1000))
-}
-
-changeRedirectNum(document.getElementById("redirectNum"))
